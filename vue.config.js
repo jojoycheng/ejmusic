b2111382a17ddb12757cc88ec379c60c0cf0fbb0
Joy Cheng <jojoycheng@gmail.com>
2022-06-12 21:13:48 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/ejmusic/' // 資料夾路徑，同時是儲存庫名稱
+  : '/'
 })
